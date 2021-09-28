0cc4ef70c09708942b5ae9938caa1b25a05a46ed
Abru Dev <80353919+abrilzucchi@users.noreply.github.com>
2021-09-28 20:24:07 -0300

    Principal Commit

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,39 @@
 <template>
- <div class="container">
+  <div class="container">
     <div class="row">
-      <h1>Criptos</h1>
+      <h1>Coin Market</h1>
 
-<table class="table table-dark">
-  <thead>
-    <tr>
-      <th v-for="title in titles" :key="title">
-        {{title}}
-      </th>
-    </tr>
-  </thead>
-    <tbody>
-      <tr v-for="(coin, index) in coins" :key="coin.id">
-        <td class="text-muted">
-          {{ index + 1 }}
-        </td>
-        <td>
-          <img :src="coin.image" style="width:2rem" class="me-2">
-          <span>
-            {{coin.name}}
-          </span>
-          <span class="ms-2 text-uppercase text-muted">
-            {{coin.symbol}}
+      <input
+        type="text"
+        class="form-control text-light bg-dark rounded-0 border-0 my-4"
+        placeholder="Search"
+        v-model="textSearch"
+        @keyup="searchCoin()"
+        autofocus
+      />
+
+      <table class="table table-hover table-dark text-light">
+        <thead>
+          <tr>
+            <th v-for="(title, index) in titles" :key="index">
+              {{ title }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(coin, index) in filteredCoins" :key="coin.id">
+            <td class="text-muted">{{ index }}</td>
+            <td>
+              <img :src="coin.image" :alt="coin.name" style="width: 2rem" class="me-2" />
+              <span>
+                {{ coin.name }}
               </span>
-              </td>
-              <td>
-                ${{coin.current_price}}
+              <span class="ms-2 text-muted text-uppercase">
+                {{ coin.symbol }}
+              </span>
             </td>
-             <td
+            <td>{{ coin.current_price.toLocaleString() }}</td>
+            <td
               :class="[
                 coin.price_change_percentage_24h > 0
                   ? 'text-success'
@@ -37,9 +42,7 @@
             >
               {{ coin.price_change_percentage_24h }}
             </td>
-            <td>
-              $ {{coin.total_volume.toLocaleString()}} 
-            </td>
+            <td>{{ coin.total_volume.toLocaleString() }}</td>
           </tr>
         </tbody>
       </table>
@@ -49,20 +52,34 @@
 
 <script>
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  components: {},
+  data() {
     return {
       coins: [],
+      filteredCoins: [],
       titles: ["#", "Coin", "Price", "Price Change", "24h Volume"],
+      textSearch: "",
     };
   },
   async mounted() {
-   const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=btc&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-   const data = await res.json()
-   console.log(data)
-   this.coins = data
-  }
-}
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    const data = await res.json();
+    this.coins = data;
+    this.filteredCoins = data;
+  },
+  methods: {
+    searchCoin() {
+      this.filteredCoins = this.coins.filter(
+        (coin) =>
+          coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+          coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())
+      );
+    },
+  },
+};
 </script>
 
 <style>
